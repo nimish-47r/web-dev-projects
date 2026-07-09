abe3a70dbc508eea25bc48f9af6903d5fc7ddde0
Nimish <nimishramteke21@gmail.com>
2026-07-09 19:54:41 +0530

    Add simple calculator

---
+++ simple-calculator/script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function clearMostRecent(input) {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = display.value.replaceAll("÷", "/");
+        display.value = display.value.replaceAll("×", "*");
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
